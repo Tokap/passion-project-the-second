4be828d90e23ce4cc99f704e71d868071aa97a8b
Patrick Allen <allenpatrick86@gmail.com>
2016-06-25 15:35:15 -0700

    add initial models

---
+++ today-i-feel/db/seeds.rb
@@ -0,0 +1,14 @@
+#### EMOTIONS SEED #####
+
+happy = Emotion.create(name: "Happy")
+sad = Emotion.create(name: "Sad")
+numb = Emotion.create(name: "Numb")
+angry = Emotion.create(name: "Angry")
+lost = Emotion.create(name: "Lost")
+
+#### QUOTES SEED ######
+
+twain = Quote.create(body: "Do the right thing. It will gratify some people and astonish the rest.", author: "Mark Twain", emotion: lost)
+twain = Quote.create(body: "Whenever you find yourself on the side of the majority, it is time to pause and reflect.", author: "Mark Twain", emotion: lost)
+
+ 
