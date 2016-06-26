b4e0d4676048ba7390af0635994256267a4067a4
Patrick Allen <allenpatrick86@gmail.com>
2016-06-26 15:16:02 -0700

    clean up code

--- today-i-feel/app/controllers/emotions_controller.rb
+++ today-i-feel/app/controllers/emotions_controller.rb
@@ -18,14 +18,20 @@ class EmotionsController < ApplicationController
     if user_signed_in?
       ## QUOTES
       @quotes = Quote.where(user_id: 1, emotion: @emotion)
+      @user_content = Quote.where(user: current_user, emotion: @emotion)
+      @videos = @videos.concat(@user_content) if !@user_content.empty?
       @quotes = @quotes.sample(4) #randomize quotes with max 4
 
       ## VIDEOS
       @videos = Video.where(user_id: 1, emotion: @emotion)
-      @videos = @videos.sample(4) #randomize videos with max 3
+      @user_content = Video.where(user: current_user, emotion: @emotion)
+      @videos = @videos.concat(@user_content) if !@user_content.empty?
+      @videos = @videos.sample(3) #randomize videos with max 3
       
       ## IMAGES
       @images = Image.where(user_id: 1, emotion: @emotion)
+      @user_content = Image.where(user: current_user, emotion: @emotion)
+      @videos = @videos.concat(@user_content) if !@user_content.empty?
       @images = @images.sample(2) #randomize videos with max 2
     
 
--- today-i-feel/app/controllers/videos_controller.rb
+++ today-i-feel/app/controllers/videos_controller.rb
@@ -8,9 +8,12 @@ class VideosController < ApplicationController
     video = params[:video]
     emotion = Emotion.find_by(name: params[:emotion])
     #fix url to be in embedded format:
-    leading_nonsense = "https://youtu.be"
-    stripped_url = video[:embedded_url].delete leading_nonsense
-    url = "https://www.youtube.com/embed/" + stripped_url
+    starting_url = video[:embedded_url]
+    leading_nonsense = "https://youtu.be".length
+    total_length = starting_url.length
+    stripped_url = starting_url[leading_nonsense..total_length]
+    # stripped_url = video[:embedded_url]
+    url = "https://www.youtube.com/embed" + stripped_url
 
     final_hash = { name: video[:name], embedded_url: url, emotion: emotion, user: current_user }
     @video = Video.create(final_hash)
