1712666f2313c6aa4e3a10af5c020b14be6395fb
Patrick Allen <allenpatrick86@gmail.com>
2016-06-26 14:54:47 -0700

    add feature to allow users to add their own youtube videos

--- today-i-feel/app/controllers/videos_controller.rb
+++ today-i-feel/app/controllers/videos_controller.rb
@@ -1,13 +1,24 @@
 class VideosController < ApplicationController
 # before_action :authenticate_user!
   def new
+    @video = Video.new
   end  
 
   def create
-    playground = params[]
+    video = params[:video]
+    emotion = Emotion.find_by(name: params[:emotion])
+    #fix url to be in embedded format:
+    leading_nonsense = "https://youtu.be"
+    stripped_url = video[:embedded_url].delete leading_nonsense
+    url = "https://www.youtube.com/embed/" + stripped_url
 
-
-    final_hash = {name: params[:name] }
+    final_hash = { name: video[:name], embedded_url: url, emotion: emotion, user: current_user }
+    @video = Video.create(final_hash)
+    if @video.valid?
+      redirect_to root_path
+    else
+      "ahhhhh!"
+    end
   end
 
 end
