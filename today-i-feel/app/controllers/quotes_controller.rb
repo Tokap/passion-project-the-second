2c1316d2aa539d7746b211274b8642a21594473d
Patrick Allen <allenpatrick86@gmail.com>
2016-06-26 15:46:44 -0700

    Implement add quote feature with secure params

--- today-i-feel/app/controllers/quotes_controller.rb
+++ today-i-feel/app/controllers/quotes_controller.rb
@@ -1,9 +1,29 @@
 class QuotesController < ApplicationController
 # before_action :authenticate_user!
-  def new
-  end  
+def new
+  @quotes = Quote.new
+end  
 
-  def create
+def create
+  quote = params[:quote]
+  emotion = Emotion.find_by(name: params[:emotion])
+  final_hash = { emotion: emotion, user: current_user }
+
+  @quote = Quote.new(quotes_params)
+  @quote.update(final_hash)
+
+  if @quote.valid?
+    @quote.save
+    redirect_to root_path
+  else
+    "ahhhhh!"
+  end
+end
+
+
+private
+  def quotes_params
+    params.require(:quote).permit(:body, :author)
   end
 
 end
